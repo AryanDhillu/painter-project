fd66bcddc6c3a4f467f12433685e750bb2514b45
AryanDhillu <dhilluaryan7@gmail.com>
2025-08-16 11:30:36 +0530

    get reviews updated

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -3,7 +3,7 @@ const Review = require('../models/review.model');
 exports.getApprovedReviews = async (req, res) => {
   try {
     // Find only the reviews where isApproved is true
-    const reviews = await Review.find({ isApproved: true });
+    const reviews = await Review.find();
     res.status(200).json(reviews);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching reviews' });
