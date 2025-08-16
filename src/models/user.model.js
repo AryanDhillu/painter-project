ffe47581ec1614f92ab1efa27b285a4ffddfb92c
AryanDhillu <dhilluaryan7@gmail.com>
2025-08-17 02:21:00 +0530

    admin role added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, trim: true, lowercase: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, enum: ['Owner', 'Staff'] },
+  role: { type: String, required: true, enum: ['Owner', 'Staff', 'Admin'] },
   createdAt: { type: Date, default: Date.now }
 });
 
