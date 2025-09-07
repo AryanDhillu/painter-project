5a719b6dd71b8d85f291f463c871be6899028a6a
AryanDhillu <dhilluaryan7@gmail.com>
2025-09-07 20:34:56 +0530

    receiptants error delete

--- src/controllers/quote.admin.controller.js
+++ src/controllers/quote.admin.controller.js
@@ -1,6 +1,5 @@
 const Quote = require('../models/quote.model');
 const { sendAppointmentEmail } = require('../services/emailService');
-const { deleteImageFromFirebase } = require('../services/firebaseService');
 
 // @desc    Get all quotes for the admin panel
 exports.getAdminQuotes = async (req, res) => {
@@ -44,30 +43,6 @@ exports.updateQuote = async (req, res) => {
     res.json(updatedQuote);
   } catch (err) {
     res.status(500).json({ message: 'Server Error', error: err.message });
-  }
-};
-
-// @desc    Delete a quote and its associated images from Firebase
-exports.deleteQuote = async (req, res) => {
-  try {
-    // 1. Find the quote in MongoDB to get the image URLs
-    const quote = await Quote.findById(req.params.id);
-    if (!quote) {
-      return res.status(404).json({ message: 'Quote not found' });
-    }
-
-    // 2. If there are images, delete them from Firebase Storage
-    if (quote.images && quote.images.length > 0) {
-      const deletePromises = quote.images.map(url => deleteImageFromFirebase(url));
-      await Promise.all(deletePromises);
-    }
-
-    // 3. After handling images, delete the quote from MongoDB
-    await Quote.findByIdAndDelete(req.params.id);
-
-    res.json({ message: 'Quote and associated images deleted successfully.' });
-  } catch (err) {
-    res.status(500).json({ message: 'Server Error' });
   }
 };
 
