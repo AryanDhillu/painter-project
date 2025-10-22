dea661c5671315467d1662979fab9791c6db653e
AryanDhillu <dhilluaryan7@gmail.com>
2025-10-22 21:27:09 +0530

    gallery field added

--- src/models/galleryItem.model.js
+++ src/models/galleryItem.model.js
@@ -4,6 +4,12 @@ const galleryItemSchema = new mongoose.Schema({
   imageUrl: { type: String, required: true },
   caption: { type: String },
   serviceType: { type: String },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true // Good for consistency
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
